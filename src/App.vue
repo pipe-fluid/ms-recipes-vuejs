ae6163a8c1c1075778a439a90b27335e563be411
felipe-pereira-lima <felipe2@Felipes-Mini.home>
2021-11-01 14:37:55 +0000

    vuetify added

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="scss">
+@import "@/assets/styling/base/main.scss";
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
-import vuetify from "./plugins/vuetify"
+import vuetify from './plugins/vuetify'
+// import vuetify from "./plugins/vuetify"
 
 Vue.config.productionTip = false;
 
 new Vue({
-  vuetify,
+  // vuetify,
   store,
-  render: (h) => h(App),
+
+  vuetify,
+  render: (h) => h(App)
 }).$mount("#app");
