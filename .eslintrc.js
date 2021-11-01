d7d7b9c461dba604ee5b447d8d4726a1e22a51a4
felipe-pereira-lima <felipe2@Felipes-Mini.home>
2021-11-01 13:57:46 +0000

    created vue-cli app

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 8,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
